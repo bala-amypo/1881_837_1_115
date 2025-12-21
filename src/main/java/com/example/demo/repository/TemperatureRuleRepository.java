723020d4c8bf44a6cab87ebd5d8fa87c07ea90a8
bala-amypo <bala@amypo.in>
2025-12-21 12:30:53 +0000

    User commit

---
+++ src/main/java/com/example/demo/repository/TemperatureRuleRepository.java
@@ -0,0 +1,8 @@
+package com.example.demo.repository;
+
+import com.example.demo.entity.TemperatureRule;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TemperatureRuleRepository
+        extends JpaRepository<TemperatureRule, Long> {
+}
