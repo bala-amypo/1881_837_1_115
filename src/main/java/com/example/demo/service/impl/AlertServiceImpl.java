ee130b116350033c91f1628059078096a38ad502
bala-amypo <bala@amypo.in>
2025-12-26 08:23:36 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/impl/AlertServiceImpl.java
@@ -0,0 +1,28 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.entity.AlertRecord;
+import com.example.demo.repository.AlertRecordRepository;
+import com.example.demo.service.AlertService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service   
+public class AlertServiceImpl implements AlertService {
+
+    private final AlertRecordRepository alertRepo;
+
+    public AlertServiceImpl(AlertRecordRepository alertRepo) {
+        this.alertRepo = alertRepo;
+    }
+
+    @Override
+    public AlertRecord triggerAlert(AlertRecord alert) {
+        return alertRepo.save(alert);
+    }
+
+    @Override
+    public List<AlertRecord> getAlertsByShipment(Long shipmentId) {
+        return alertRepo.findByShipmentId(shipmentId);
+    }
+}
---
+++ src/main/java/com/example/demo/service/impl/BreachDetectionServiceImpl.java
@@ -0,0 +1,36 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.entity.BreachRecord;
+import com.example.demo.repository.BreachRecordRepository;
+import com.example.demo.service.BreachDetectionService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+@Service
+public class BreachDetectionServiceImpl implements BreachDetectionService {
+
+    private final BreachRecordRepository breachRepo;
+
+    public BreachDetectionServiceImpl(BreachRecordRepository breachRepo) {
+        this.breachRepo = breachRepo;
+    }
+
+    @Override
+    public BreachRecord logBreach(BreachRecord breach) {
+        return breachRepo.save(breach);
+    }
+
+    @Override
+    public BreachRecord resolveBreach(Long id) {
+        BreachRecord breach = breachRepo.findById(id).orElseThrow();
+        breach.setResolved(true);
+        return breachRepo.save(breach);
+    }
+
+    @Override
+    public List<BreachRecord> getBreachesByShipment(Long shipmentId) {
+        return breachRepo.findByShipmentId(shipmentId);
+    }
+}
